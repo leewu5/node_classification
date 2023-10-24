63e3231c55e261215b5c3ef086ea8e310d64addc
leewu5 <leewu5@163.com>
2023-10-24 15:27:23 +0800

    first commit

---
+++ data.py
@@ -0,0 +1,111 @@
+import os
+import os.path as osp
+import pickle
+import itertools
+import scipy.sparse as sp
+import urllib
+from collections import namedtuple
+import numpy as np
+
+Data = namedtuple('Data', ['x', 'y', 'adjacency_dict',
+                           'train_mask', 'val_mask', 'test_mask'])
+
+class CoraData(object):
+    download_url = "https://github.com/kimiyoung/planetoid/raw/master/data"
+    filenames = ["ind.cora.{}".format(name) for name in
+                 ['x', 'tx', 'allx', 'y', 'ty', 'ally', 'graph', 'test.index']]
+
+    def __init__(self, data_root="cora", rebuild=False):
+        self.data_root = data_root
+        save_file = osp.join(self.data_root, "processed_cora.pkl")
+        if osp.exists(save_file) and not rebuild:
+            print("Using Cached file: {}".format(save_file))
+            self._data = pickle.load(open(save_file, "rb"))
+        else:
+            self.maybe_download()
+            self._data = self.process_data()
+            with open(save_file, "wb") as f:
+                pickle.dump(self.data, f)
+            print("Cached file: {}".format(save_file))
+
+    @property
+    def data(self):
+        return self._data
+
+    def process_data(self):
+        print("Process data ...")
+        _, tx, allx, y, ty, ally, graph, test_index = [self.read_data(
+            osp.join(self.data_root, "raw", name)) for name in self.filenames]
+        train_index = np.arange(y.shape[0])
+        val_index = np.arange(y.shape[0], y.shape[0] + 500)
+        sorted_test_index = sorted(test_index)
+
+        x = np.concatenate((allx, tx), axis=0)
+        y = np.concatenate((ally, ty), axis=0).argmax(axis=1)
+
+        x[test_index] = x[sorted_test_index]
+        y[test_index] = y[sorted_test_index]
+        num_nodes = x.shape[0]
+
+        train_mask = np.zeros(num_nodes, dtype=np.bool)
+        val_mask = np.zeros(num_nodes, dtype=np.bool)
+        test_mask = np.zeros(num_nodes, dtype=np.bool)
+        train_mask[train_index] = True
+        val_mask[val_index] = True
+        test_mask[test_index] = True
+        adjacency_dict = graph
+        print("Node's feature shape: ", x.shape)
+        print("Node's label shape: ", y.shape)
+        print("Adjacency's shape: ", len(adjacency_dict))
+        print("Number of training nodes: ", train_mask.sum())
+        print("Number of validation nodes: ", val_mask.sum())
+        print("Number of test nodes: ", test_mask.sum())
+
+        return Data(x=x, y=y, adjacency_dict=adjacency_dict,
+                    train_mask=train_mask, val_mask=val_mask, test_mask=test_mask)
+
+    def maybe_download(self):
+        save_path = os.path.join(self.data_root, "raw")
+        for name in self.filenames:
+            if not osp.exists(osp.join(save_path, name)):
+                self.download_data(
+                    "{}/{}".format(self.download_url, name), save_path)
+
+    @staticmethod
+    def build_adjacency(adj_dict):
+        """根据邻接表创建邻接矩阵"""
+        edge_index = []
+        num_nodes = len(adj_dict)
+        for src, dst in adj_dict.items():
+            edge_index.extend([src, v] for v in dst)
+            edge_index.extend([v, src] for v in dst)
+        # 去除重复的边
+        edge_index = list(k for k, _ in itertools.groupby(sorted(edge_index)))
+        edge_index = np.asarray(edge_index)
+        adjacency = sp.coo_matrix((np.ones(len(edge_index)),
+                                   (edge_index[:, 0], edge_index[:, 1])),
+                                  shape=(num_nodes, num_nodes), dtype="float32")
+        return adjacency
+
+    @staticmethod
+    def read_data(path):
+        name = osp.basename(path)
+        if name == "ind.cora.test.index":
+            out = np.genfromtxt(path, dtype="int64")
+            return out
+        else:
+            out = pickle.load(open(path, "rb"), encoding="latin1")
+            out = out.toarray() if hasattr(out, "toarray") else out
+            return out
+
+    @staticmethod
+    def download_data(url, save_path):
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+        data = urllib.request.urlopen(url)
+        filename = os.path.split(url)[-1]
+
+        with open(os.path.join(save_path, filename), 'wb') as f:
+            f.write(data.read())
+
+        return True
---
+++ sampling.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def sampling(src_nodes, sample_num, neighbor_table):
+
+    results = []
+    for sid in src_nodes:
+        # 从节点的邻居中进行有放回地进行采样
+        res = np.random.choice(neighbor_table[sid], size=(sample_num,))
+        results.append(res)
+    return np.asarray(results).flatten()
+
+
+def multihop_sampling(src_nodes, sample_nums, neighbor_table):
+
+    sampling_result = [src_nodes]
+    for k, hopk_num in enumerate(sample_nums):
+        hopk_result = sampling(sampling_result[k], hopk_num, neighbor_table)
+        sampling_result.append(hopk_result)
+    return sampling_result
