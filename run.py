475d2b36bece960d4056d6eb6a25a99ef3f89c7c
leewu5 <leewu5@163.com>
2023-10-27 00:07:27 +0800

    update some files

--- run.py
+++ run.py
@@ -3,21 +3,20 @@ import numpy as np
 import torch.nn as nn
 import torch.optim as optim
 from network import GraphSage
-from data import CoraData
+from data import data
 from sampling import multihop_sampling
 from collections import namedtuple
 
 Data = namedtuple('Data', ['x', 'y', 'adjacency_dict',
                            'train_mask', 'val_mask', 'test_mask'])
 
-data = CoraData().data
 x = data.x / data.x.sum(1, keepdims=True)  # 归一化数据，使得每一行和为1
 
 train_index = np.where(data.train_mask)[0]
 train_label = data.y[train_index]
 test_index = np.where(data.test_mask)[0]
 
-INPUT_DIM = 1433    # 输入维度
+INPUT_DIM = 1    # 输入维度
 # Note: 采样的邻居阶数需要与GCN的层数保持一致
 HIDDEN_DIM = [128, 7]   # 隐藏单元节点数
 NUM_NEIGHBORS_LIST = [10, 10]   # 每阶采样邻居的节点数
@@ -30,7 +29,7 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 model = GraphSage(input_dim=INPUT_DIM, hidden_dim=HIDDEN_DIM,
                   num_neighbors_list=NUM_NEIGHBORS_LIST).to(DEVICE)
-print(model)
+#print(model)
 criterion = nn.CrossEntropyLoss().to(DEVICE)
 optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=5e-4)
 
